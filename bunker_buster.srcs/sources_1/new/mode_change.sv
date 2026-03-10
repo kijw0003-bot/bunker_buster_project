39c8b060ade9fb55fe17b8e67bd247914f3d6aaf
KJW <kijw0003@gmail.com>
2026-03-10 18:10:40 +0900

    xpr추가

---
+++ bunker_buster.srcs/sources_1/new/mode_change.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2026/03/10 18:05:59
+// Design Name: 
+// Module Name: mode_change
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module mode_change(
+
+    );
+endmodule
