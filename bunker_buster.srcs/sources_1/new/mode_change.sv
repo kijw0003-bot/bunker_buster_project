9f622742f00a52c0acc1ffbb4c30d4149100168f
KJW <kijw0003@gmail.com>
2026-03-10 21:28:00 +0900

    top 에 선들 연결 후 gamemode 생성

--- bunker_buster.srcs/sources_1/new/mode_change.sv
+++ bunker_buster.srcs/sources_1/new/mode_change.sv
@@ -3,13 +3,46 @@
 
 
 module mode_change (
+    input  logic clk,
     input  logic reset,
     input  logic mode_btn,
-    output logic mux_sel
-    //  output logic 
+    output logic game_mode
 );
 
+    typedef enum {
+        OFF,
+        ON
+    } state_t;
 
+    state_t c_state, n_state;
+
+    always_ff @(posedge clk or posedge reset) begin
+        if (reset) begin
+            c_state <= OFF;
+        end else begin
+            c_state <= n_state;
+        end
+    end
+
+    always_comb begin
+        n_state   = c_state;
+        game_mode = 0;
+        case (c_state)
+            OFF: begin
+                game_mode = 0;
+                if (mode_btn) begin
+                    n_state = ON;
+                end
+            end
+            ON: begin
+                game_mode = 1;
+                if (mode_btn) begin
+                    n_state = OFF;
+                end
+
+            end
+        endcase
+    end
 
 
 endmodule
