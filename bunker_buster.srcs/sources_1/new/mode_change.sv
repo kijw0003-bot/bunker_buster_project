7e256741e53871b753a8cbde8dec877b73283d13
KJW <kijw0003@gmail.com>
2026-03-10 18:20:05 +0900

    모듈 생성

--- bunker_buster.srcs/sources_1/new/mode_change.sv
+++ bunker_buster.srcs/sources_1/new/mode_change.sv
@@ -1,26 +1,8 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 2026/03/10 18:05:59
-// Design Name: 
-// Module Name: mode_change
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 
-module mode_change(
 
-    );
+module mode_change (
+    input logic sw
+);
 endmodule
